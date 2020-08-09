7d5518cf829cba27627df81106b56d95a4e4b363
Melissa.Wong <melissa.wong@jhuapl.edu>
2020-08-09 13:55:06 -0400

    Change to side by side layout

--- Shiny_App/Two_Means/app.R
+++ Shiny_App/Two_Means/app.R
@@ -19,62 +19,67 @@ ui <- fluidPage(
 
     # Application title
     titlePanel("Comparing Two Means"),
-    
-    # General Description
-    p("A t-test is the typical statistical test used to compare two population
+    fluidRow(
+        column(6,
+               # General Description
+               p("A t-test is the typical statistical test used to compare two population
       means. However, it's instructive to see that this same comparison can
       be thought of in terms of Analysis of Variance (ANOVA), linear regression,
        or a Bayesian estimation problem."),
-    
-    p("Adjust the slider bars to change the mean, standard 
+               
+               p("Adjust the slider bars to change the mean, standard 
                deviation or number of random 
-                    samples drawn from each population."),
-
-    fluidRow(
-        column(4,
-            # Inputs for X
-            sliderInput("meanX", "X Mean", min=-5, max=5, value=0),
-            sliderInput("sdX", "X Std Dev", min=0.5, max=10, value=1)
-            ),
-        column(4,
-            # Inputs for y
-            sliderInput("meanY", "Y Mean", min=-5, max=5, value=1),
-            sliderInput("sdY", "Y Std Dev", min=0.5, max=10, value=1)
-            ),
-        column(4,
-               # Number of samples
-               sliderInput("n", "Number of Samples", min=5, max=100, value=30))
-    ),
-    
-    fluidRow(plotOutput("distPlot", height="200px")),
-    
-    p("Select each tab to see the different ways this comparison can
+                    samples drawn from each population, and then observe
+                 how these changes affect the results obtained from each method."),
+               
+               fluidRow(
+                   column(4,
+                          # Inputs for X
+                          sliderInput("meanX", "X Mean", min=-5, max=5, value=0),
+                          sliderInput("sdX", "X Std Dev", min=0.5, max=10, value=1)
+                   ),
+                   column(4,
+                          # Inputs for y
+                          sliderInput("meanY", "Y Mean", min=-5, max=5, value=1),
+                          sliderInput("sdY", "Y Std Dev", min=0.5, max=10, value=1)
+                   ),
+                   column(4,
+                          # Number of samples
+                          sliderInput("n", "Number of Samples", min=5, max=100, value=30))
+               ),
+               
+               fluidRow(plotOutput("distPlot", height="200px"))
+               ),
+        column(6,
+               p("Select each tab to see the different ways this comparison can
        be formulated."),
-    
-    tabsetPanel(
-        tabPanel("t-Test",
-                 p("There are two types of t-tests, pooled and 
+               
+               tabsetPanel(
+                   tabPanel("t-Test",
+                            p("There are two types of t-tests, pooled and 
                    Welch's.  The pooled t-test is appropriate when
                    the population variances are approximately equal and is generally 
                    robust if there aren't many outliers. Welch's 
                    test is appropriate if the variances are not equal; 
                    however it gives the same results as the pooled test 
                    when the variances and sample sizes are equal. Results 
-                   for a two-sided test with 95% for both methods are
+                   for a two-sided test with 95% confidence for both methods are
                    shown below."),
-
-                 verbatimTextOutput("tTestResults")
-                 ),
-        tabPanel("ANOVA"),
-        tabPanel("Linear Regression",
-                 p("Some words here..."),
-                 
-                 fluidRow(column(6, verbatimTextOutput("lrResults")),
-                          column(6, plotOutput("lrPlot")))
-                 ),
-        tabPanel("Bayesian Estimation")
+                            
+                            verbatimTextOutput("tTestResults")
+                   ),
+                   tabPanel("ANOVA"),
+                   tabPanel("Linear Regression",
+                            p("Some words here..."),
+                            
+                            verbatimTextOutput("lrResults"),
+                            plotOutput("lrPlot")
+                   ),
+                   tabPanel("Bayesian Estimation")
+               )
+               )
     )
-    
+  
 )
 
 # Define server logic required to draw a histogram
