dcc2ba8b8e55cfd611ed04adbc32558dbd25c1fb
Melissa.Wong <dispagna3@gmail.com>
2020-08-19 17:12:43 -0400

    Minor updates to random effects example

--- Shiny_App/Random_Effects/app.R
+++ Shiny_App/Random_Effects/app.R
@@ -17,12 +17,14 @@ ui <- fluidPage(
     # Application title
     titlePanel("Fixed vs. Random Effects"),
     
-    p("I really struggled with understanding at a conceptual level what the differences were
-      between fixed and random effects when first introduced to the concepts in a Design of
-      Experiments course. In this example, I try to give a more intuitive understanding of 
-      the differences between the two."),
-    p("Imagine you've bought multiple boxes of three brands of cereal--Cheerios, Froot Loops
-      and Raisin Bran. Further, imagine you have a device that can measure the actual
+    p("In this example, I try to give a more intuitive understanding of 
+      the differences between fixed and random effects without diving into the equations
+      for expected mean squares, sum of squares, etc. There are plenty of references which
+      show how to derive those equations; for me the challenge was understanding the 
+      differences at a conceptual level when first learning about analyis of variance
+      (ANOVA)."),
+    p("Imagine you bought multiple boxes of three brands of cereal--Cheerios, Froot Loops
+      and Raisin Bran. Further, imagine you have a device which can measure the actual
       sugar content per serving in each box (as opposed to the value on the
       nutritional label). You could do either a fixed effect or a random effect ANOVA on
       the measured sugar content data. Which do you choose to do and why?"),
@@ -42,7 +44,7 @@ ui <- fluidPage(
                     sliderInput("sdCheerios", "Cheerios", min=1, max=4, value=4, step=0.1),
                                sliderInput("sdFrootLoops", "Froot Loops", min=1, max=4, value=4, step=0.1),
                                sliderInput("sdRaisinBran", "Raisin Bran", min=1, max=4, value=4, step=0.1),
-                               sliderInput("n", "Number of Boxes per Brand", min=3, max=15, value=5, step=1)),
+                               sliderInput("n", "Number of Boxes per Brand", min=3, max=15, value=3, step=1)),
              column(9,plotOutput("distPlot", height = "200px"),
                     helpText("ANOVA Output"),
                       verbatimTextOutput("aov"))),
@@ -74,8 +76,8 @@ server <- function(input, output) {
             HTML(paste(
             "Treating Brand as a fixed effect means you are asking the question ",
             "<B>Is there
-             a statistically significant difference in the average sugar content per
-             serving between Cheerios, Froot Loops and Raisin Bran?</B>"
+             a statistically significant difference in the average sugar content
+             between Cheerios, Froot Loops and Raisin Bran?</B>"
             ))
         else
             HTML(paste(
@@ -88,16 +90,18 @@ server <- function(input, output) {
     output$details <- renderUI({
         if(input$fixedEffect)
             HTML(paste(
-                "Observe that with a small number of samples and large variance for sugar content
+                "Observe that with a small number of samples and large variance in sugar content
             for each brand it is difficult to determine whether or not there is a difference.
             As you increase the sample size and decrease the variance it becomes easier to
-            see that the mean sugar content is significantly different between the three brands."
+            see that the mean sugar content is different between the three brands."
             ))
         else
             HTML(paste(
                 "In the ANOVA output, the Brand random effect is the variance
                 <i>between</i> brands and the Residuals random effect is the variance
-                <i>within</i> brands. Also, notice the fixed effect Intercept term
+                <i>within</i> brands. As you change the number of samples and variance
+                for each brand, observe how that affects the variance between and within
+                brands. Also, notice the fixed effect Intercept term
                 at the very bottom of the ANOVA output--
                 that is the estimated mean sugar content for all brands of cereal."
             ))    
